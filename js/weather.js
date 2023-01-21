f984a6405cb35c4b13ada01db43a390ae45f23a1
naim <rlaskdla97@gmail.com>
2023-01-21 21:28:24 +0900

    0121 utilize Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,19 @@
+const API_KEY="3f7ae7d510ddcd3e72caefc9f220769b"
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const long = position.coords.longitude;
-    console.log("you live in" + lat + long);
+    const lon = position.coords.longitude;
+    const url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+        const city = document.querySelector("#weather span:first-child")
+        const weather = document.querySelector("#weather span:last-child")
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
+    });
+
 }
 
 function onGeoError() {
